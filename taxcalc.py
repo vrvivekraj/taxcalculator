abff0b55f47e846f3501b5db244a54e5b51f1bff
vrvivekraj <vivekrajvr@gmail.com>
2025-03-06 21:45:13 +0530

    Update taxcalc.py

--- taxcalc.py
+++ taxcalc.py
@@ -1,10 +1,4 @@
 import streamlit as st
-import streamlit_analytics
-
-# Start analytics tracking
-streamlit_analytics.start_tracking()
-st.title("Take-Home Salary Calculator (FY 2025-26)")
-
 
 def format_currency(amount):
     """
